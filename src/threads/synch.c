1a58a632c457572c246090becbbde5241019a789
haojin <2678432321@qq.com>
2021-03-05 01:10:58 +0800

    priority-fifo

--- src/threads/synch.c
+++ src/threads/synch.c
@@ -71,7 +71,7 @@ sema_down (struct semaphore *sema)
   old_level = intr_disable ();
   while (sema->value == 0) 
     {
-	  list_push_back (&sema->waiters, &thread_current ()->elem);
+	  list_push_front (&sema->waiters, &thread_current ()->elem);
       thread_block ();
     }
   sema->value--;
